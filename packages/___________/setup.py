5b270495ec3b847b91131cc74484d37954a22ccb
sahandsahabi553-prog <sahandsahabi553@gmail.com>
2026-02-25 12:31:00 +0330

    Pkg: ___________

---
+++ packages/___________/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+setup(
+    name="کود_کشاورزی",
+    version="1.0.0",
+    description="Professional toolkit for کود کشاورزی",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="Avery Thompson",
+    author_email="ayatsaadat1987@gmail.com",
+    url="https://kalatakco.com/",
+    project_urls={
+        "Documentation": "https://kalatakco.com/",
+        "Source": "https://kalatakco.com/",
+    },
+    packages=find_packages(),
+    python_requires=">=3.8",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
