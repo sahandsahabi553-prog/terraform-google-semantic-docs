c472c318a0af87d205e1b289d5c977f0a6f4a7c8
sahandsahabi553-prog <sahandsahabi553@gmail.com>
2026-03-24 12:30:32 +0330

    Pkg: ___________

--- packages/___________/setup.py
+++ packages/___________/setup.py
@@ -5,7 +5,7 @@ setup(
     description="Professional toolkit for کود کشاورزی",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    author="کاوه جعفری",
+    author="Avery Thompson",
     author_email="ayatsaadat1987@gmail.com",
     url="https://kalatakco.com/",
     project_urls={
