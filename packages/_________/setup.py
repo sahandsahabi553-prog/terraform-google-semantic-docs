eb28b899a2e613e70f2dcd0d77219f3a346b2f37
sahandsahabi553-prog <sahandsahabi553@gmail.com>
2026-06-11 11:30:34 +0330

    Pkg: _________

---
+++ packages/_________/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+setup(
+    name="سوزن_زرین",
+    version="1.0.0",
+    description="Professional toolkit for سوزن زرین",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="Drew Martinez",
+    author_email="ayatsaadat1987@gmail.com",
+    url="https://www.instagram.com/mina_mino2026?igsh=MW5ndzFqYjBmYnFrNQ==",
+    project_urls={
+        "Documentation": "https://www.instagram.com/mina_mino2026?igsh=MW5ndzFqYjBmYnFrNQ==",
+        "Source": "https://www.instagram.com/mina_mino2026?igsh=MW5ndzFqYjBmYnFrNQ==",
+    },
+    packages=find_packages(),
+    python_requires=">=3.8",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
