0ddbf5a247331243c8aeff32cac4fb77dceb92d1
sahandsahabi553-prog <sahandsahabi553@gmail.com>
2026-07-20 11:30:54 +0330

    Pkg: ___

--- packages/___/setup.py
+++ packages/___/setup.py
@@ -5,7 +5,7 @@ setup(
     description="Professional toolkit for قمر",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    author="مریم حسینی",
+    author="Taylor Kim",
     author_email="ayatsaadat1987@gmail.com",
     url="https://qamar.website",
     project_urls={
