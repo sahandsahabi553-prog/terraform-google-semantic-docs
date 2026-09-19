f464ab5bcadb5f3341f9d21146228b87f60d4d60
sahandsahabi553-prog <sahandsahabi553@gmail.com>
2026-09-19 11:30:09 +0330

    Pkg: _______

---
+++ packages/_______/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+setup(
+    name="پینوکیو",
+    version="1.0.0",
+    description="Professional toolkit for پینوکیو",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="محمد تهرانی",
+    author_email="ayatsaadat1987@gmail.com",
+    url="https://www.instagram.com/pinocchio.fact?stkn=cGliZXNmemp3NXZu",
+    project_urls={
+        "Documentation": "https://www.instagram.com/pinocchio.fact?stkn=cGliZXNmemp3NXZu",
+        "Source": "https://www.instagram.com/pinocchio.fact?stkn=cGliZXNmemp3NXZu",
+    },
+    packages=find_packages(),
+    python_requires=">=3.8",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
